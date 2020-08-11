26f4fd1cb9a41999d8d02af1d7bd1df63889648f
FilipMaciejowski <filip.maciejowski@gmail.com>
2020-08-11 21:57:43 +0200

    episodes list assigned

---
+++ src/components/episodesList/episodesList.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+import classes from "./EpisodesList.module.css"
+
+const EpisodesList = () => {
+   
+    return (
+        <div className={classes.EposidesList__main}>
+            <ul>
+                <li>
+                
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default EpisodesList
