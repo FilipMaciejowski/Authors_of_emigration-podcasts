2b13360579dc2c15aa7a9f18416c8cbd43106ff1
FilipMaciejowski <filip.maciejowski@gmail.com>
2020-08-16 22:22:54 +0200

    buttons new design

--- src/components/podcastElement/podcastElement.js
+++ src/components/podcastElement/podcastElement.js
@@ -37,9 +37,9 @@ const PodcastElement = ({
           {description}
         </p>
         <div className={classes.PodcastElement__content_listen}>
-          <div>
+          <div className={classes.Listen__button}>
             <p>Posłuchaj</p>
-            <div className={classes.Arrow__container}>
+            {/* <div className={classes.Arrow__container}>
               <svg
                 className={classes.Arrow}
                 xmlns="http://www.w3.org/2000/svg"
@@ -53,17 +53,16 @@ const PodcastElement = ({
                   d="M38.354 4.354a.5.5 0 000-.708L35.172.464a.5.5 0 10-.707.708L37.293 4l-2.828 2.828a.5.5 0 10.707.708l3.182-3.182zM0 4.5h38v-1H0v1z"
                 />
               </svg>
-            </div>
-          </div>
-          <Link to={`/podcasts/${page}`}>
-            <div className={classes.Headphones__container}>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="2 0 20 20">
+            </div> */}
+            
+            <Link to={`/podcasts/${page}`}>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="2 .5 20 20" className={classes.Headphones} >
                 <defs />
                 <path fill="none" d="M0 0h24v24H0z" opacity="none" />
-                <path d="M12 1a9 9 0 00-9 9v7c0 1.66 1.34 3 3 3h3v-8H5v-2c0-3.87 3.13-7 7-7s7 3.13 7 7v2h-4v8h3c1.66 0 3-1.34 3-3v-7a9 9 0 00-9-9z" />
+                <path fill="grgb(85, 85, 85)" d="M12 1a9 9 0 00-9 9v7c0 1.66 1.34 3 3 3h3v-8H5v-2c0-3.87 3.13-7 7-7s7 3.13 7 7v2h-4v8h3c1.66 0 3-1.34 3-3v-7a9 9 0 00-9-9z" />
               </svg>
-            </div>
           </Link>
+          </div>
         </div>
       </div>
     )
