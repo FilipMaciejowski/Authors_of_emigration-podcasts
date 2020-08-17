431564ca12c80185f206c6b755f982527bfba6f8
FilipMaciejowski <filip.maciejowski@gmail.com>
2020-08-17 10:59:56 +0200

    wire added to headphones icon

--- src/components/podcastElement/podcastElement.js
+++ src/components/podcastElement/podcastElement.js
@@ -56,12 +56,15 @@ const PodcastElement = ({
                 />
               </svg>
             </div> */}
+            <svg className={classes.Wire} width="46" height="21" viewBox="0 0 40 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M0 12.2524C7.30597 12.2524 6.34227 11.8653 8.5 5.99999C10.6577 0.134723 13.8553 0.0648427 18 9.49999C22.1447 18.9351 24.3867 -0.44566 29.6053 3.07266C34.8238 6.59098 26.0527 20.4123 45 18.8823" stroke="black" stroke-width="2"/>
+</svg>
+
             
-           
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="2 .5 20 20" className={classes.Headphones} >
                 <defs />
                 <path fill="none" d="M0 0h24v24H0z" opacity="none" />
-                <path fill="grgb(85, 85, 85)" d="M12 1a9 9 0 00-9 9v7c0 1.66 1.34 3 3 3h3v-8H5v-2c0-3.87 3.13-7 7-7s7 3.13 7 7v2h-4v8h3c1.66 0 3-1.34 3-3v-7a9 9 0 00-9-9z" />
+                <path fill="" d="M12 1a9 9 0 00-9 9v7c0 1.66 1.34 3 3 3h3v-8H5v-2c0-3.87 3.13-7 7-7s7 3.13 7 7v2h-4v8h3c1.66 0 3-1.34 3-3v-7a9 9 0 00-9-9z" />
               </svg>
               </Link>
           </div>
