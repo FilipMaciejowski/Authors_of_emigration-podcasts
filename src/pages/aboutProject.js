39d2152b1306baa3f5eeb561a8318a293d4e205c
Filip Mac Datek <filip@datek.no>
2023-01-25 22:39:48 +0100

    new text added in about page

--- src/pages/aboutProject.js
+++ src/pages/aboutProject.js
@@ -30,27 +30,10 @@ const AboutProject = (data) => {
     render={ data => (<PodcastTemplate aboutProject={true}>
         <h1 style={styles[0]}>O projekcie:</h1>
         <p style={styles2[1]}>
-          Strona została stworzona na potrzeby realizacji trzymiesięcznego
-          programu stypendialnego Ministerstwa Kultury i Dziedzictwa Narodowego
-          „Kultura w sieci”. Zamieszczane w kolejnych tygodniach podcasty będą
-          dotyczyć ważnych postaci polskiej kultury emigracyjnej, o których
-          opowiedzą badacze ich twórczości oraz najbliżsi. Dowiemy się jaki
-          wpływ miało doświadczenie emigracji m. in. dla Gustawa
-          Herlinga-Grudzińskiego, Jana Lebensteina, Zbigniewa Herberta, Józefa
-          Czapskiego, Jacka Bierezina, Konstantego Jeleńskiego, Romana
-          Cieślewicza oraz wielu innych szerzej nieznanych polskiemu odbiorcy
-          artystów, np. Pawła Jocza, Macieja Niemca, Tadeusza Brudzyńskiego.
+          Strona została stworzona na potrzeby realizacji trzymiesięcznego programu stypendialnego Ministerstwa Kultury i Dziedzictwa Narodowego „Kultura w sieci”. Zamieszczane w kolejnych tygodniach podcasty dotyczyły ważnych postaci polskiej kultury emigracyjnej, o których opowiadali badacze ich twórczości oraz najbliżsi. Dowiadujemy się z nich jaki wpływ miało doświadczenie emigracji m. in. dla Gustawa Herlinga-Grudzińskiego, Jana Lebensteina, Zbigniewa Herberta, Józefa Czapskiego, Jacka Bierezina, Konstantego Jeleńskiego, Romana Cieślewicza oraz wielu innych szerzej nieznanych polskiemu odbiorcy artystów, np. Pawła Jocza, Macieja Niemca, Tadeusza Brudzyńskiego.
         </p>
         <p style={styles2[1]}>
-          Rozmowy są rejestrowane najczęściej przy użyciu internetowych
-          komunikatorów, niekiedy z gośćmi, którzy wciąż przebywają poza
-          granicami Polski. W kontekście panującej pandemii ten bardzo ostatnio
-          popularny sposób porozumiewania się zyskuje wymiar poniekąd
-          symboliczny – emigracja oddalała bowiem od siebie przyjaciół,
-          rodziny, zmuszała do kontaktu jednymi możliwymi kanałami. Celem
-          przedsięwzięcia jest przede wszystkim popularyzacja wiedzy na temat
-          polskiej kultury emigracyjnej i dokumentacja wspomnień na temat
-          wymienionych osób.
+        Rozmowy były rejestrowane przy użyciu internetowych komunikatorów, niekiedy z gośćmi, którzy wciąż przebywają poza granicami Polski. W kontekście pandemii ten bardzo popularny wówczas sposób porozumiewania się zyskał wymiar poniekąd symboliczny – emigracja oddalała bowiem od siebie przyjaciół, rodziny, zmuszała do kontaktu jednymi możliwymi sposobami. Celem przedsięwzięcia była przede wszystkim popularyzacja wiedzy na temat polskiej kultury emigracyjnej i dokumentacja wspomnień na temat wymienionych osób.
         </p>
         <Ministry />
         <h1 style={styles[0]}>O autorze:</h1>
